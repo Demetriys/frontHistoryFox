1d6267d4d80f503e33590fa8a9cdbd8b5e1a66a9
Pro <kuhardmitro@gmail.com>
2024-02-08 15:42:28 +0200

    +

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     return(
         <>
             <h1>Homepage</h1>
-            <Link href="/calc">Calc</Link> <hr/>
+            <Link href="/calc"> Calc</Link> <hr/>
             <Link href="/counter">Counter</Link> <hr/>
             <Link href="/clients">Navigate to clients</Link> <hr/>
         </>
